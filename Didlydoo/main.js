414ddf05610d7655cd90b7ee1728a18b91d67d5c
thomas <scozzari.thomas@yahoo.fr>
2024-01-24 09:58:21 +0100

    thomas

--- Didlydoo/main.js
+++ Didlydoo/main.js
@@ -6,6 +6,8 @@ let myArray = [];
 let InputTitle = document.getElementById('input-title')
 let InputDescription = document.getElementById('input-description')
 let InputDate = document.getElementById('input-date')
+let InputUser = document.getElementById('input-user') 
+let myTaskList = document.getElementById('list-container')
 
 myButton.addEventListener("click", () =>{
     myModal.style.display = "flex"
@@ -24,32 +26,47 @@ let addingTask = document.getElementById('modal-button')
 addingTask.addEventListener("click", () => {
     let myTitle = InputTitle.value
     let myDescription = InputDescription.value
+    let myUser = InputUser.value
     let myDate = InputDate.value
 
     myArray.push({
         title: myTitle,
         description: myDescription,
+        user: myUser,
         date: myDate
     })
-    console.log(myArray)
+    console.table(myArray)
 
     myModal.style.display = "none"
     myButton.style.display = "block"
+    myTaskList.style.display = 'block'
 
     InputTitle.value = ""
     InputDescription.value = ""
     InputDate.value = ""
+    InputUser.value = ""
 
-    createTaskList(myTitle)
+    createTaskList(myTitle, myDescription, myUser)
 })
 
 console.log(myArray)
 
-function createTaskList(title, description){
-    let myTaskList = document.getElementById('list-container')
+function createTaskList(title, description, user){
     let myFirstTask = document.createElement('div')
 
     myFirstTask.classList = "task"
-    myFirstTask.innerText = title, description
+    myFirstTask.innerText = title
     myTaskList.appendChild(myFirstTask)
+
+    let myTaskDescription = document.createElement('div')
+
+    myTaskDescription.classList = "description"
+    myTaskDescription.innerText = description
+    myFirstTask.appendChild(myTaskDescription)
+
+    let myTaskUser = document.createElement('div')
+
+    myTaskUser.classList = "user"
+    myTaskUser.innerText = user
+    myFirstTask.appendChild(myTaskUser)
 }
